3bee8625cce2018c4df632ab1891a861a8286af4
LionelMessiKB100 <99144412+pradolionel@users.noreply.github.com>
2024-09-28 12:22:51 -0500

    Update import.hx

--- source/import.hx
+++ source/import.hx
@@ -14,6 +14,18 @@ import llua.Lua;
 import backend.Achievements;
 #end
 
+#if android
+import android.content.Context as AndroidContext;
+import android.widget.Toast as AndroidToast;
+import android.os.Environment as AndroidEnvironment;
+import android.Permissions as AndroidPermissions;
+import android.Settings as AndroidSettings;
+import android.Tools as AndroidTools;
+import android.os.Build.VERSION as AndroidVersion;
+import android.os.Build.VERSION_CODES as AndroidVersionCode;
+import android.os.BatteryManager as AndroidBatteryManager;
+#end
+
 #if sys
 import sys.*;
 import sys.io.*;
